484a398a25d2d440842eedb2577dc8ab1b7cfc9a
Nychollaas <nychollaspietro260@gmail.com>
2026-08-17 16:18:53 -0300

    atualiza classe turma

--- server.js
+++ server.js
@@ -14,3 +14,19 @@ let atleta1 = new Atleta("Cypher", 19);
 
 atleta1.registrar();
 
+
+class Turma {
+    constructor(nomeTurma, idadeTurma) {
+        this.nomeTurma = nomeTurma;
+        this.idadeTurma = idadeTurma;
+    }
+
+    registrar() {
+        console.log("Turma registrada:", this.nomeTurma);
+        console.log("Idade:", this.idadeTurma);
+    }
+}
+
+let turma1 = new Turma("Cypher", 19);
+
+turma1.registrar();
